b7089650277b389e34b845e50ed4ef8bfc7863a9
Masum Hasan <masumhasan92@gmail.com>
2021-03-20 15:00:06 +0600

    aq

--- ll.c
+++ ll.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
+
 int main()
 {
-    int i;
-    for (i = 1; i <= 10; ++i)
+    int a, b, c, i;
+    printf("Enter Lower Range : ");
+    scanf("%d", &a);
+    printf("Enter Upper Range : ");
+    scanf("%d", &b);
+    printf("The perfect numbers are : ");
+    for (i = a; i <= b; i++)
     {
-        printf("%d", i);
+        int sum = 0;
+        for (c = 1; c < i; c++)
+        {
+            if (i % c == 0)
+            {
+                sum += c;
+            }
+        }
+        if (sum == i)
+        {
+            printf("%d ", i);
+        }
     }
-
     return 0;
 }
